a4c46690fc4b9c6c936d5175acad6541483e7ccc
uijinson <uijinson159@gmail.com>
2021-02-02 23:33:08 +0900

    first commit

---
+++ src/test/java/com/uijin/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uijin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
